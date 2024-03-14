0fd6deaf8b840235ea1eb3dc4602a431057b53e0
Sergey Mudretskiy <mudreckii-sergei@yandex.ru>
2024-03-14 20:45:25 +1200

    re

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,36 +1,130 @@
-import './Auth.css';
-import { NavLink } from 'react-router-dom';
-import useForm from '../../utils/useForm';
-import headerLogo from '../../images/logo.svg';
+import React from 'react';
+import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 
-export default function Login({ onLogin }) {
+import Logo from '../Logo/Logo';
 
-  const { values, errors, handleChange, isValid } = useForm();
+import './Login.css';
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    if (!values.email || !values.password) {
-      return;
+function Login({ onLogin, loginError }) {
+  const [userData, setUserData] = React.useState({
+    email: '',
+    password: '',
+  });
+
+  const [emailDirty, setEmailDirty] = React.useState(false);
+  const [passwordDirty, setPasswordDirty] = React.useState(false);
+
+  const [emailError, setEmailError] = React.useState('Email не может быть пустым');
+  const [passwordError, setPasswordError] = React.useState('Пароль не может быть пустым');
+
+  const [formValid, setFormValid] = React.useState(false);
+
+  React.useEffect(() => {
+    if (emailError || passwordError) {
+      setFormValid(false);
+    } else {
+      setFormValid(true);
     }
-    onLogin(values);
+  }, [emailError, passwordError]);
+
+  const handleChange = (evt) => {
+    const { name, value } = evt.target;
+    setUserData({
+      ...userData,
+      [name]: value,
+    });
+
+    switch (name) {
+      case 'email':
+        const reEmail =
+          /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/i;
+        if (String(value).length === 0) {
+          setEmailError('Email не может быть пустым');
+        } else if (!value.match(reEmail)) {
+          setEmailError('Необходимо указать e-mail в формате name@domain.zone');
+        } else {
+          setEmailError('');
+        }
+        break;
+      case 'password':
+        if (String(value).length === 0) {
+          setPasswordError('Пароль не может быть пустым');
+        } else {
+          setPasswordError('');
+        }
+        break;
+    }
+  };
+
+  const handleSubmit = (evt) => {
+   evt.preventDefault();
+console.log(userData)
+    onLogin(userData);
+  };
+
+  const blurHandler = (evt) => {
+    switch (evt.target.name) {
+      case 'email':
+        setEmailDirty(true);
+        break;
+      case 'password':
+        setPasswordDirty(true);
+        break;
+    }
   };
 
   return (
-    <main className='auth'>
-      <NavLink to='/'><img src={headerLogo} className="auth__logo" alt="лого" /></NavLink>
-      <div className='auth__container'>
-        <h1 className='auth__title'>Рады видеть!</h1>
-        <form className='auth__form form' onSubmit={handleSubmit}>
-          <label className='auth__label' htmlFor='email'>E-mail</label>
-          <input type="email" className='auth__input' name='email' id='email' required value={values.email || ''} pattern={"[a-zA-Z0-9_.]+@[a-zA-Z0-9_]+\\.[a-z]{2,}"} onChange={handleChange}/>
-          <span className='auth__error'>{errors.email}</span>
-          <label className='auth__label' htmlFor='password'>Пароль</label>
-          <input type="password" className='auth__input' name='password' id='password' required minLength={3} value={values.password || ''} onChange={handleChange}/>
-          <span className='auth__error'>{errors.password}</span>
-          <button type='submit' className='auth__button-login' disabled={!isValid}>Войти</button>
-        </form>
-        <p className='auth__subtitle'>Еще не зарегистрированы?<a href="/signup" className='auth__link'>Регистрация</a></p>
-      </div>
-    </main>
-  )
+    <section className='login'>
+      <Logo />
+      <h1 className='login__title'>Рады видеть!</h1>
+      <form className='login__form' onSubmit={handleSubmit}>
+        <fieldset className='login__container'>
+          <label className='login__label' for='email'>
+            E-mail
+          </label>
+          <input
+            required
+            value={userData.email}
+            onChange={handleChange}
+            onBlur={blurHandler}
+            name='email'
+            className='login__input'
+            type='email'
+            id='email'
+            placeholder='Введите ваш E-mail'
+          ></input>
+        </fieldset>
+        {emailDirty && emailError && <span className='login__error'>{emailError}</span>}
+        <fieldset className='login__container'>
+          <label className='login__label' for='password'>
+            Пароль
+          </label>
+          <input
+            required
+            value={userData.password}
+            onChange={handleChange}
+            onBlur={blurHandler}
+            name='password'
+            className='login__input'
+            type='password'
+            id='password'
+            placeholder='Введите ваш пароль'
+          ></input>
+        </fieldset>
+        {passwordDirty && passwordError && <span className='login__error'>{passwordError}</span>}
+        <input type='submit' disabled={!formValid} className='login__submitButton hover-button' value='Войти' />
+          
+        
+      </form>
+      <p className='login__text'>
+        Еще не зарегистрированы?&nbsp;
+        <Link className='login__link' to='/signup'>
+          Регистрация
+        </Link>
+      </p>
+    </section>
+  );
 }
+
+export default Login;
--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,39 +1,165 @@
+import React from 'react';
 
+import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import Logo from '../Logo/Logo';
+import './Register.css';
 
-import '../Login/Auth.css';
+function Register({ onRegister }) {
+  const [userData, setUserData] = useState({
+    email: '',
+    password: '',
+    name: '',
+  });
 
-import useForm from '../../utils/useForm';
+  const [nameDirty, setNameDirty] = useState(false);
+  const [emailDirty, setEmailDirty] = useState(false);
+  const [passwordDirty, setPasswordDirty] = useState(false);
 
+  const [nameError, setNameError] = useState('Имя не может быть пустым');
+  const [emailError, setEmailError] = useState('Email не может быть пустым');
+  const [passwordError, setPasswordError] = useState('Пароль не может быть пустым');
 
-export default function Register({ onRegister }) {
+  const [formValid, setFormValid] = useState(false);
 
-  const { values, errors, handleChange, isValid } = useForm();
+  const handleChange = (evt) => {
+    const { name, value } = evt.target;
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    onRegister(values);
+    setUserData({
+      ...userData,
+      [name]: value,
+    });
+
+    switch (name) {
+      case 'email':
+        const reEmail =
+          /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/i;
+        if (String(value).length === 0) {
+          setEmailError('Email не может быть пустым');
+        } else if (!value.match(reEmail)) {
+          setEmailError('Необходимо указать e-mail в формате name@domain.zone');
+        } else {
+          setEmailError('');
+        }
+        break;
+      case 'name':
+        const reName = /^[a-яёa-z -]{2,30}$/i;
+        if (String(value).length === 0) {
+          setNameError('Имя не может быть пустым');
+        } else if (String(value).length === 1) {
+          setNameError('Имя должно быть не короче 2 символов');
+        } else if (!value.match(reName)) {
+          setNameError('Имя должно содержать только латиницу, кириллицу, пробел и дефис');
+        } else {
+          setNameError('');
+        }
+        break;
+      case 'password':
+        if (String(value).length === 0) {
+          setPasswordError('Пароль не может быть пустым');
+        } else {
+          setPasswordError('');
+        }
+        break;
+    }
   };
 
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+
+    onRegister(userData);
+  };
+
+  const blurHandler = (evt) => {
+    switch (evt.target.name) {
+      case 'name':
+        setNameDirty(true);
+        break;
+      case 'email':
+        setEmailDirty(true);
+        break;
+      case 'password':
+        setPasswordDirty(true);
+        break;
+    }
+  };
+
+  useEffect(() => {
+    if (nameError || emailError || passwordError) {
+      setFormValid(false);
+    } else {
+      setFormValid(true);
+    }
+  }, [nameError, emailError, passwordError]);
+
   return (
-    <main className='auth'>
-     <Logo />
-      <div className='auth__container'>
-        <h1 className='auth__title'>Добро пожаловать!</h1>
-        <form className='auth__form form' onSubmit={handleSubmit}>
-          <label className='auth__label' htmlFor='name'>Имя</label>
-          <input type="text" className='auth__input' name='name' id='name' required minLength={2} value={values.name || ''} pattern={"^[A-Za-z][A-Za-z0-9_]{1,29}$"} onChange={handleChange} />
-          <span className='auth__error'>{errors.name}</span>
-          <label className='auth__label' htmlFor='email'>E-mail</label>
-          <input type="email" className='auth__input' name='email' id='email' required value={values.email || ''} pattern={"[a-zA-Z0-9_.]+@[a-zA-Z0-9_]+\\.[a-z]{2,}"} onChange={handleChange} />
-          <span className='auth__error'>{errors.email}</span>
-          <label className='auth__label' htmlFor='password'>Пароль</label>
-          <input type="password" className='auth__input' name='password' id='password' required minLength={3} value={values.password || ''} onChange={handleChange} />
-          <span className='auth__error'>{errors.password}</span>
-          <button type='submit' className='auth__button-register' disabled={!isValid}>Зарегистрироваться</button>
-        </form>
-        <p className='auth__subtitle'>Уже зарегистрированы?<a href="/signin" className='auth__link'>Войти</a></p>
-      </div>
-    </main>
-  )
+    <section className='register'>
+      <Logo />
+      <h1 className='register__title'>Добро пожаловать!</h1>
+      <form className='register__form' onSubmit={handleSubmit}>
+        <fieldset className='register__container'>
+          <label className='register__label' htmlFor='name'>
+            Имя
+          </label>
+          <input
+            required
+            value={userData.name}
+            onChange={handleChange}
+            onBlur={blurHandler}
+            name='name'
+            className='register__input'
+            type='text'
+            id='name'
+            placeholder='Введите ваше имя'
+          ></input>
+        </fieldset>
+        {nameDirty && nameError && <span className='register__error'>{nameError}</span>}
+        <fieldset className='register__container'>
+          <label className='register__label' htmlFor='email'>
+            E-mail
+          </label>
+          <input
+            required
+            value={userData.email}
+            onChange={handleChange}
+            onBlur={blurHandler}
+            name='email'
+            className='register__input'
+            type='email'
+            id='email'
+            placeholder='Введите ваш E-mail'
+          ></input>
+        </fieldset>
+        {emailDirty && emailError && <span className='register__error'>{emailError}</span>}
+        <fieldset className='register__container'>
+          <label className='register__label' htmlFor='password'>
+            Пароль
+          </label>
+          <input
+            required
+            value={userData.password}
+            onChange={handleChange}
+            onBlur={blurHandler}
+            name='password'
+            className='register__input'
+            type='password'
+            id='password'
+            placeholder='Введите ваш пароль'
+          ></input>
+        </fieldset>
+        {passwordDirty && passwordError && <span className='register__error'>{passwordError}</span>}
+        <button disabled={!formValid} className='register__submitButton hover-button'>
+          Зарегистрироваться
+        </button>
+      </form>
+      <p className='register__text'>
+        Уже зарегистрированы?&nbsp;
+        <Link className='register__link' to='/signin'>
+          Войти
+        </Link>
+      </p>
+    </section>
+  );
 }
+
+export default Register;
